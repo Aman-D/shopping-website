c5780d653c054e05ab9139a7eb3ebe3e24c73d75
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-02-11 17:48:10 +0530

    added the router to homepage component

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import "./menu-item.style.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`} >
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+
+  < div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
     <div style={{ backgroundImage: `url(${imageUrl})` }} className="background-image"></div>
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">Buy Now</span>
     </div>
-  </div>
+  </div >
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
