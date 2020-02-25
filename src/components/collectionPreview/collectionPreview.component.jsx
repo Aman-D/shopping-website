699b759e6e912a9eb12d8313275376c7c34d7b84
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-02-25 09:18:41 +0530

    updating shop page,collection-preview and collection item component

---
+++ src/components/collectionPreview/collectionPreview.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.componenet'
+import './collectionPreview.style.scss'
+
+const CollectionPreview = ({ title, items }) => (
+    < div className="collection-preview" >
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+            {
+                items
+                    .filter((item, index) => index < 4)
+                    .map(({ id, ...otherCollectionProps }) => (
+                        <CollectionItem key={id} {...otherCollectionProps} />
+                    ))
+            }
+
+        </div>
+    </div >
+)
+
+export default CollectionPreview;
