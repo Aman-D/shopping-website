65ee30affd49786dd8508cfd30859f94170c76d9
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-04-06 12:08:41 +0530

    button fix

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './custom-button.styles.scss';
+import React from "react";
+import "./custom-button.styles.scss";
 
 const CustomButton = ({ children, inverted, google, ...otherProps }) => (
-    <button className={`${inverted ? 'inverted' : ''} ${google ? 'google' : ''} custom-button`} {...otherProps}>
-        {children}
-    </button>
-)
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      google ? "google" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
