33a4f47d3967f0fc0c3f24b36d85fa5b17b3aba7
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-04-23 13:39:15 +0530

    seperated the frontend to client folder add added node backend to handel stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const stripePrice = price * 100;
   const publishKey = "pk_test_79VOEetptZBG6keJc6umqHEd00iTo6L9YV";
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: stripePrice,
+        token,
+      },
+    })
+      .then((response) => alert("Payment Successful"))
+      .catch((err) => {
+        console.log(JSON.parse(err));
+        alert("Payment was unsuccessful. PLease check your card details");
+      });
   };
   return (
     <StripeCheckout
